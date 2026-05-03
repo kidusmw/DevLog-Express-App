ed1979a7297bfef611f474fec2988fab40914002
Kidus <kiduswubie7110@gmail.com>
2026-05-03 12:29:36 +0300

    Write service layer for CRUD changelog

---
+++ src/services/changelog.ts
@@ -0,0 +1,58 @@
+import { prisma } from "../lib/prisma";
+
+export async function getAllChangeLogs() {
+  return prisma.changelog.findMany({
+    include: {
+      author: {
+        select: { id: true, email: true },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
+
+export async function getAllChangeLogById(id: number) {
+  const changelog = prisma.changelog.findUnique({
+    where: { id },
+    include: {
+      author: {
+        select: { id: true, email: true },
+      },
+    },
+  });
+
+  if (!changelog) {
+    throw new Error("Changelog not found");
+  }
+
+  return changelog;
+}
+
+export async function createChangelog(
+  title: string,
+  version: string,
+  description: string,
+  authorId: number,
+) {
+  return prisma.changelog.create({
+    data: { title, version, description, authorId },
+  });
+}
+
+export async function updateChangeLog(
+  id: number,
+  data: Partial<{ title: string; version: string; description: string }>,
+) {
+  return prisma.changelog.update({
+    where: { id },
+    data,
+  });
+}
+
+export async function deleteChangeLog(id: number) {
+  return prisma.changelog.delete({
+    where: { id },
+  });
+}
