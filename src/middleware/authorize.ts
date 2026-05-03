91644b48c9975f6b2d73021c0dd05381c518886b
Kidus <kiduswubie7110@gmail.com>
2026-05-03 13:42:53 +0300

    No need for using async

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from "express";
 import { AuthRequest } from "./auth";
 
-export async function authorize(...roles: string[]) {
+export function authorize(...roles: string[]) {
   return (req: AuthRequest, res: Response, next: NextFunction) => {
     if (!req.user || !roles.includes(req.user.role)) {
       return res.status(403).json({
